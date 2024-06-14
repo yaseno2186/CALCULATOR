4d86cbc4e6a9c029168f630e710e1f250b0cd206
Yass Almardod <149265907+yaseno2186@users.noreply.github.com>
2024-06-14 18:10:04 +0200

    Update 0.1

--- src/Jui.java
+++ src/Jui.java
@@ -4,107 +4,83 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Jui extends JFrame {
-    String s0, s1, s2;
-    static JTextField num1Field = new JTextField();
+    String FirstNumber, theRule, SecondNumber;
+    static JTextField NumberField = new JTextField();
 
 
     public Jui() {
         super("Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        s0 = s1 = s2 = "";
+        FirstNumber = theRule = SecondNumber = "";
 
-        JPanel panelText = new JPanel();
-        panelText.setLayout(new GridLayout(1, 1, 1, 1));
-        panelText.setBorder(new EmptyBorder(10, 10, 10, 10));
-
-
-        Font font = new Font("Arial", Font.BOLD, 30);
-        Font font1 = new Font("Arial", Font.PLAIN, 30);
-        font1.isItalic();
-        num1Field.setEditable(false);
-        num1Field.setFont(font);
-        num1Field.setBackground(Color.gray);
-        num1Field.setHorizontalAlignment(JTextField.CENTER);
-        panelText.add(num1Field);
+        JPanel PanelForField = new JPanel();
+        PanelForField.setLayout(new GridLayout(1, 1, 1, 1));
+        PanelForField.setBorder(new EmptyBorder(10, 10, 10, 10));
 
 
-        JPanel panelNum = new JPanel();
+        Font FieldFont = new Font("Arial", Font.BOLD, 30);
+        Font ButtonsFont = new Font("Montserrat", Font.PLAIN, 40);
+        ButtonsFont.isItalic();
+        NumberField.setEditable(false);
+        NumberField.setFont(FieldFont);
+        NumberField.setBackground(Color.gray);
+        NumberField.setHorizontalAlignment(JTextField.CENTER);
+        PanelForField.add(NumberField);
 
-        panelNum.setLayout(new GridLayout(4, 2, 0, 3));
-        panelNum.setBorder(new EmptyBorder(0, 10, 5, 10));
 
-        ArrayList<JButton> list = new ArrayList<>();
+        JPanel PanelForButtons = new JPanel();
 
+        PanelForButtons.setLayout(new GridLayout(3, 2, 0, 3));
+        PanelForButtons.setBorder(new EmptyBorder(0, 10, 5, 10));
 
-
-        JButton b1 = new JButton("1");
-        list.add(b1);
+        ArrayList<JButton> ListOfButtons = new ArrayList<>();
 
         JButton b2 = new JButton("2");
-        list.add(b2);
-
+        JButton b1 = new JButton("1");
         JButton b3 = new JButton("3");
-        list.add(b3);
-
-        JButton bMinus = new JButton("-");
-        list.add(bMinus);
-
         JButton b4 = new JButton("4");
-        list.add(b4);
-
         JButton b5 = new JButton("5");
-        list.add(b5);
-
         JButton b6 = new JButton("6");
-        list.add(b6);
-
-        JButton bPlus = new JButton("+");
-        list.add(bPlus);
-
         JButton b7 = new JButton("7");
-        list.add(b7);
-
         JButton b8 = new JButton("8");
-        list.add(b8);
-
         JButton b9 = new JButton("9");
-        list.add(b9);
-
-        JButton bTimes = new JButton("x");
-        list.add(bTimes);
-
-        JButton bDelete = new JButton("C");
-        list.add(bDelete);
-
         JButton b0 = new JButton("0");
-        list.add(b0);
-
-        JButton bEqual = new JButton("=");
-        list.add(bEqual);
-
+        JButton bMinus = new JButton("-");
+        JButton bPlus = new JButton("+");
+        JButton bTimes = new JButton("x");
         JButton bDivid = new JButton("÷");
-        list.add(bDivid);
+        JButton bDelete = new JButton("C");
+        JButton bPoint = new JButton(".");
+        JButton bDel = new JButton("⌫");
+        JButton bEqual = new JButton("=");
+
+
+        // Add all buttons to the list at once
+        Collections.addAll(ListOfButtons,
+                b1, b2, b3, bMinus, bPlus,bEqual,
+                b4, b5, b6, bTimes, bDivid,bDel,
+                b7, b8, b9,b0,bPoint,bDelete);
 
 
         Color color = Color.white;
-
-        for (JButton button : list) {
+        for (JButton button : ListOfButtons) {
             button.setForeground(color);
             button.setBackground(Color.darkGray);
-            button.setFont(font1);
+            button.setFont(ButtonsFont);
             button.addActionListener(theAction);
-            panelNum.add(button);
+            PanelForButtons.add(button);
 
         }
 
-        add(panelText, BorderLayout.NORTH);
-        add(panelNum, BorderLayout.CENTER);
+        add(PanelForField, BorderLayout.NORTH);
+        add(PanelForButtons, BorderLayout.CENTER);
 
         pack();
-        setSize(400, 500);
+        setSize(600, 500);
         setVisible(true);
     }
 
@@ -116,77 +92,87 @@ public class Jui extends JFrame {
     ActionListener theAction = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String s = e.getActionCommand();
-            if ((s.charAt(0) >= '0' && s.charAt(0) <= '9') || s.charAt(0) == '.') {
-                if (!s1.equals(""))
-                    s2 = s2 + s;
-                else
-                    s0 = s0 + s;
+            String Input = e.getActionCommand();
 
-                num1Field.setText(s0 + s1 + s2);
-            } else if (s.charAt(0) == 'C') {
+
+            if ((Input.charAt(0) >= '0' && Input.charAt(0) <= '9') || Input.charAt(0) == '.') {
+                if (!theRule.equals(""))
+                    SecondNumber = SecondNumber + Input;
+                else
+                    FirstNumber = FirstNumber + Input;
+
+                NumberField.setText(FirstNumber + theRule + SecondNumber);
+            } else if (Input.charAt(0) == 'C') {
                 // clear the one letter
-                s0 = s1 = s2 = "";
+                FirstNumber = theRule = SecondNumber = "";
 
                 // set the value of text
-                num1Field.setText(s0 + s1 + s2);
-            } else if (s.charAt(0) == '=') {
+                NumberField.setText(FirstNumber + theRule + SecondNumber);
+            } else if (Input.charAt(0) == '⌫') {
+                if (theRule.isEmpty() && SecondNumber.isEmpty()) {
+                    FirstNumber = FirstNumber.replaceAll(".$", "");
+                } else if (!FirstNumber.isEmpty() && SecondNumber.isEmpty()) {
+                    theRule = theRule.replaceAll(".$", "");
+                } else {
+                    SecondNumber = SecondNumber.replaceAll(".$", "");
+                }
+                // set the value of text
+                NumberField.setText(FirstNumber + theRule + SecondNumber);
+
+            } else if (Input.charAt(0) == '=') {
 
                 double te;
 
                 // store the value in 1st
-                if (s1.equals("+"))
-                    te = (Double.parseDouble(s0) + Double.parseDouble(s2));
-                else if (s1.equals("-"))
-                    te = (Double.parseDouble(s0) - Double.parseDouble(s2));
-                else if (s1.equals("÷"))
-                    te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+                if (theRule.equals("+"))
+                    te = (Double.parseDouble(FirstNumber) + Double.parseDouble(SecondNumber));
+                else if (theRule.equals("-"))
+                    te = (Double.parseDouble(FirstNumber) - Double.parseDouble(SecondNumber));
+                else if (theRule.equals("÷"))
+                    te = (Double.parseDouble(FirstNumber) / Double.parseDouble(SecondNumber));
                 else
-                    te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+                    te = (Double.parseDouble(FirstNumber) * Double.parseDouble(SecondNumber));
 
 
                 // set the value of text
-                num1Field.setText(s0 + s1 + s2 + "=" + te);
+                NumberField.setText(FirstNumber + theRule + SecondNumber + "=" + te);
 
                 // convert it to string
-                s0 = Double.toString(te);
+                FirstNumber = Double.toString(te);
 
-                s1 = s2 = "";
+                theRule = SecondNumber = "";
             } else {
                 // if there was no operand
-                if (s1.equals("") || s2.equals(""))
-                    s1 = s;
+                if (theRule.equals("") || SecondNumber.equals(""))
+                    theRule = Input;
                     // else evaluate
                 else {
                     double te;
 
                     // store the value in 1st
-                    if (s1.equals("+"))
-                        te = (Double.parseDouble(s0) + Double.parseDouble(s2));
-                    else if ( s1.equals("-"))
-                        te = (Double.parseDouble(s0) - Double.parseDouble(s2));
-                    else if (s1.equals("÷"))
-                        te = (Double.parseDouble(s0) / Double.parseDouble(s2));
+                    if (theRule.equals("+"))
+                        te = (Double.parseDouble(FirstNumber) + Double.parseDouble(SecondNumber));
+                    else if (theRule.equals("-"))
+                        te = (Double.parseDouble(FirstNumber) - Double.parseDouble(SecondNumber));
+                    else if (theRule.equals("÷"))
+                        te = (Double.parseDouble(FirstNumber) / Double.parseDouble(SecondNumber));
                     else
-                        te = (Double.parseDouble(s0) * Double.parseDouble(s2));
+                        te = (Double.parseDouble(FirstNumber) * Double.parseDouble(SecondNumber));
 
                     // convert it to string
-                    s0 = Double.toString(te);
+                    FirstNumber = Double.toString(te);
 
                     // place the operator
-                    s1 = s;
+                    theRule = Input;
 
                     // make the operand blank
-                    s2 = "";
+                    SecondNumber = "";
                 }
 
                 // set the value of text
-                num1Field.setText(s0 + s1 + s2);
+                NumberField.setText(FirstNumber + theRule + SecondNumber);
             }
-
-
         }
 
     };
-
 }
